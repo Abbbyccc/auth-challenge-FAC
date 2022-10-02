e6043504135ef5aa45b6c7fece09c4783ee81f87
Abbbyccc <abbychien@hotmail.com>
2022-10-02 21:52:44 +0100

    finish challenge 4

--- src/routes/log-out.js
+++ src/routes/log-out.js
@@ -1,10 +1,15 @@
-function post(req, res) {
-  /**
-   * [1] Get the session ID from the cookie
-   * [2] Remove that session from the DB
-   * [3] Remove the session cookie
-   * [4] Redirect back home
-   */
-}
+const { removeSession } = require("../model/session")
 
+function post(req, res) {
+  const sid = req.signedCookies.sid;
+  removeSession(sid);
+  res.clearCookie("sid");
+  res.redirect("/");
+}
+/**
+ * [1] Get the session ID from the cookie
+ * [2] Remove that session from the DB
+ * [3] Remove the session cookie
+ * [4] Redirect back home
+ */
 module.exports = { post };
