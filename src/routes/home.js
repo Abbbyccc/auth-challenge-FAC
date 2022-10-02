7b611cf7cd532fc72e4193961d2bbc7b818c99c3
Abbbyccc <abbychien@hotmail.com>
2022-10-02 21:59:30 +0100

    change spacing

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,13 +3,7 @@ const { getSession } = require("../model/session")
 
 
 function get(req, res) {
-  /**
-   * [1] Read session ID from cookie
-   * [2] Get session from DB
-   * [3] If the session exists render a log out form
-   * [4] This should submit a request to `POST /log-out`
-   * [5] Else render the sign up/log in links
-   */
+ 
   const sid = req.signedCookies.sid;
   const session = getSession(sid);
   const title = "Confess your secrets!";
@@ -25,5 +19,11 @@ function get(req, res) {
   const body = Layout({ title, content });
   res.send(body);
 }
-
+ /**
+   * [1] Read session ID from cookie
+   * [2] Get session from DB
+   * [3] If the session exists render a log out form
+   * [4] This should submit a request to `POST /log-out`
+   * [5] Else render the sign up/log in links
+   */
 module.exports = { get };
